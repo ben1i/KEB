563b3c9b805218f0db929a186d0a17094496eb03
Benjamin <benjamin.brihaye@student.heaj.be>
2025-06-02 17:11:03 +0200

    Assets, HTML, CSS, JS, JSON
    Assets: more images + edited names for images
    
    JSON: added a JSON for "Your Role" section
    
    HTML: remove span
    
    CSS: Style for the "OIur Fight" slider
    
    JS: Code for "Our Fight" slider (ScrollTrigger)

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -2,6 +2,9 @@
 
 import { PowerGlitch } from 'powerglitch';
 import { gsap } from "gsap";
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+
+gsap.registerPlugin(ScrollTrigger);
 
 const introTransition = document.querySelector('.transition');
 const {
@@ -52,3 +55,61 @@ quitButton.addEventListener('click', function() {
     burgerMenu.classList.add('hidden');
 })
 
+const fightData = document.querySelector('.theydont__explaination');
+
+if (window.location.pathname.endsWith("fight.html")) { //ChatGPT
+    fetch('../assets/data/data.json')
+        .then(function(data) {
+            return data.json();
+        })
+        .then(function(data) {
+            data.forEach(function(item) {
+
+                let fightDiv = document.createElement('div');
+                fightDiv.classList.add('theydont__div');
+                fightDiv.classList.add('theydont__div--' + item.title.toLowerCase());
+
+
+                let fightTitle = document.createElement('h4');
+                fightTitle.textContent = item.title;
+                fightDiv.appendChild(fightTitle);
+
+                for (let i = 1; i < item.img.length + 1; i++) {
+                    let fightImage = document.createElement('img');
+                    fightImage.src = item.img[i-1];
+                    fightImage.alt = item.title + i;
+                    fightDiv.appendChild(fightImage);
+                }
+
+                fightData.appendChild(fightDiv);
+            })
+
+            // First 2 lines -> GSAP ScrollTrigger tips & mistakes
+            const img = document.querySelectorAll('img');
+            img.forEach(div => {
+                gsap.from(div, {
+                    x: -500,
+                    scrollTrigger: {
+                        trigger: div,
+                        toggleActions: "restart pause reverse pause",
+                        start: "top bottom",
+                        end: "top center",
+                        scrub: 1,
+                        //pin: header,
+                    }
+                });
+            });
+
+            /*
+            gsap.to(header, {
+                scrollTrigger: {
+                    trigger: header,
+                    toggleActions: "restart pause reverse pause",
+                    start: "top top",
+                    pin: true,
+                    pinType: "fixed",
+                }
+            })
+            */
+        });
+}
